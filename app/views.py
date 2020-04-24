67d30137ce8a9274035e1d92ca4d3a3a118e2e3e
LuisFelipeNavas <luis.navas@usantotomas.edu.co>
2020-04-23 21:46:21 -0500

    MovieRate con templates

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hola, mundo")
+
+def cat(request):
+    return render(request, 'app/cat.html')
