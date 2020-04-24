5161d81171d058e2a931f64e2f2fdcc6eef1d670
LuisFelipeNavas <luis.navas@usantotomas.edu.co>
2020-04-23 21:51:09 -0500

    MovieRate con templates

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'app' 
 urlpatterns = [
     path('', views.index, name='index'), 
-    path('cat', views.cat, name='cat'), 
+    path('cat/', views.cat, name='cat'), 
 ]
