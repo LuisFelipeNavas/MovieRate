5254fb8ecda5e30a6addb94b73b53a0fa6e4e26e
LuisFelipeNavas <luis.navas@usantotomas.edu.co>
2020-04-27 16:51:36 -0500

    Avance, index, login, register and logout

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,8 @@ app_name = 'app'
 urlpatterns = [
     path('', views.index, name='index'), 
     path('cat/', views.cat, name='cat'), 
+    path('login/', views.log_in, name='log_in'), 
+    path('register/', views.register, name='register'), 
+    path('base/', views.base, name='base'), 
+    path('prueba/', views.prueba, name='prueba'), 
 ]
--- app/views.py
+++ app/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hola, mundo")
+    return render(request, 'app/index.html')
 
 def cat(request):
     return render(request, 'app/cat.html')
+
+def log_in(request):
+    return render(request, 'app/log_in.html')
+
+def register(request):
+    return render(request, 'app/register.html')
+
+def base(request):
+    return render(request, 'app/base.html')
+
+def prueba(request):
+    return render(request, 'app/prueba.html')
