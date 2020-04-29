22314f78743e64cbfa883645c4691bd73ee5f99e
paula466 <paula.linares@usantotomas.edu.co>
2020-04-28 22:56:04 -0500

    ver cartelera, ver pelicula, ranking)

--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,12 @@ from . import views
 app_name = 'app' 
 urlpatterns = [
     path('', views.index, name='index'), 
-    path('cat/', views.cat, name='cat'), 
+    
     path('login/', views.log_in, name='log_in'), 
     path('register/', views.register, name='register'), 
     path('base/', views.base, name='base'), 
     path('prueba/', views.prueba, name='prueba'), 
+    path('cat/', views.cat, name='cat'), 
+    path('cat/badmoms.html/', views.badmoms, name='cat/badmoms'), 
+    path('cat/ranking.html/', views.ranking, name='cat/ranking'),
 ]
